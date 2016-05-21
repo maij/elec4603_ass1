b08207b63b8e35b395b564dc64c57f7b98a13f5b
Mark Johnson <mark.johnson.ai@gmail.com>
2016-05-21 18:03:07 +1000

    Various fixes. Assignment amendment provides V_BC, not V_CE, so no need to calculate V_BC. No longer calculating delta_n, as some values were clearly negative and couldn't be plotted on a log axis. Now getting full n_p values. Each of delta_n_2 and 3 are now correctly the delta values.

--- matlab/elec4603_3_c.m
+++ matlab/elec4603_3_c.m
@@ -17,21 +17,18 @@ exponent_coeff = q/(k_B * T); % Constant exponent
 
 %% Test parameters
 W_B  = 1e-3;                  % Width of base (cm)
-V_BE = 0.1;                   % Voltage across base-emitter junction (V)
-V_CE = -7;                    % Voltage across base-collector junction (V)
+V_BE = -0.1;                  % Voltage across base-emitter junction (V)
+V_BC = -7;                    % Voltage across base-collector junction (V)
 
 %% Simulation
-all_x = linspace(0, W_B, 50e3);
-V_BC = V_BE - V_CE;
-delta_n_2 = n_p0 + base_coeff*(exp(exponent_coeff * (-V_BE))-1);
-delta_n_3 = n_p0 + base_coeff*(exp(exponent_coeff * (V_BC))-1);
+x = linspace(0, W_B, 1e3);
+delta_n_2 = base_coeff*(exp(exponent_coeff * (-V_BE))-1);
+delta_n_3 = base_coeff*(exp(exponent_coeff * (V_BC))-1);
 
-common_denom   = sinh(W_B/L_n);
+denom   = sinh(W_B/L_n);
+n_p = zeros(1, size(x,2));    % Minority carrier concentration (including n_p0)
+n_p = n_p0 + delta_n_2 * (sinh((W_B - x)/L_n)/denom) + delta_n_3 * (sinh(x/L_n)/denom);
 
-for i = 1:size(all_x,2)
-    x = all_x(i);
-    delta_n(i) = delta_n_2 * (sinh((W_B - x)/L_n)/common_denom) + delta_n_3 * (sinh(x/L_n)/common_denom);
-end
 
 %% Plot results
 figure(1);
