846a6a159f654cc83f0b3a2b59031cdbc8fb896b
Mark Johnson <mark.johnson.ai@gmail.com>
2016-05-21 22:12:25 +1000

    Improved plotting prettiness, tidying up.

--- matlab/elec4603_3_c.m
+++ matlab/elec4603_3_c.m
@@ -2,7 +2,7 @@
 N_A   = 1e19;                 % Acceptor donor concentration (cm^-2)
 mu_n  = 1417;                 % Electron mobility (cm^2 / V sec)
 tau_n = 25e-9;                % Carrier lifetime (s)
-n_i   = 1e10;                % Intrinsic carrier concentration (cm^-2)
+n_i   = 1e10;                 % Intrinsic carrier concentration (cm^-2)
 
 n_p0 = 1e1;                   % Base minority carrier concentration, under T.E.
 q    = 1.602e-19;             % Electron charge (magnitude)
@@ -26,14 +26,15 @@ delta_n_2 = base_coeff*(exp(exponent_coeff * (-V_BE))-1);
 delta_n_3 = base_coeff*(exp(exponent_coeff * (V_BC))-1);
 
 denom   = sinh(W_B/L_n);
+% Calculate the minority carrier concentration for each x.
 n_p = n_p0 + delta_n_2 * (sinh((W_B - x)/L_n)/denom) + delta_n_3 * (sinh(x/L_n)/denom);
 
 
 %% Plot results
 figure(1);
-semilogy(x*1e3, n_p, 'b-');
+semilogy(x*1e3, n_p, 'b-', 'LineWidth', 1.5);
 set(gca, 'FontSize', 18);
-line([min(x)*1e3 max(x)*1e3], [n_p0 n_p0], 'LineStyle', '-.', 'Color', 'r');
+line([min(x)*1e3 max(x)*1e3], [n_p0 n_p0], 'LineWidth', 1.5, 'LineStyle', '-.', 'Color', 'r');
 title('Minority carrier concentration in base of npn transistor', ...
     'FontSize', 28);
 xlabel('Distance from base-emitter junction (\mum)', 'FontSize', 28);
@@ -42,7 +43,5 @@ leg = legend('Minority carrier concentration', 'n_{p0}');
 set(leg, 'FontSize', 18);
 
 ylim_curr = get(gca,'ylim');
-% set(gca, 'ylim', [1 ylim_curr(2)]);
 xlim([min(x), max(x)]*1e3);
 ylim([min(1, ylim_curr(1)), ylim_curr(2)]);
-% plot(delta_n)
